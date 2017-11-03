98a8d9c1f8a32358076951d426b786693d41d673
Jake <jake@Jakes-MacBook-Pro.local>
2017-11-03 13:58:51 -0700

    Isolate Game class, import board dependency.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,3 @@
-class Game {
-  constructor(numberOfRows, numberofColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberofColumns, numberOfBombs);
-  }
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
 
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Game Over!');
-      this._board.print();
-    } else if (!this._board.hasSafeTiles()) {
-      console.log('You win!');
-    } else {
-      console.log('Current Board:');
-      this._board.print();
-    }
-  }
-} // Game
 const g = new Game(3,3,3); //instantiate game class
 g.playMove(0,0);
